f13331822a2a8ba719a9c6adb77fefa1b1a14d98
Ashley Delph <115664544+AshleyDelphDevOps@users.noreply.github.com>
2023-11-15 13:23:19 +0000

    Changes made due to variables not been declared correctly

--- main.tf
+++ main.tf
@@ -1,47 +1,47 @@
 # Define an Azure Resource Group
 resource "azurerm_resource_group" "learningt3rraf0rm" {
-  name     = "var.resource_group_name" # Change to your desired resource group name
-  location = "var.location"       # Change to your desired Azure region
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Define a Virtual Network (VNet)
 resource "azurerm_virtual_network" "T3rraNet" {
-  name                = "var.vnet_name"
-  address_space       = "var.vnet_address_space" # Change to your desired address space
-  location            = azurerm_resource_group.T3rraNet.location
-  resource_group_name = azurerm_resource_group.T3rraNet.name
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.learningt3rraf0rm.location
+  resource_group_name = azurerm_resource_group.learningt3rraf0rm.name
 }
 
 # Define a subnet within the VNet
 resource "azurerm_subnet" "SubT3rra" {
-  name                 = "var.subnet_name"
-  resource_group_name  = azurerm_resource_group.SubT3rra.name
-  virtual_network_name = azurerm_virtual_network.SubT3rra.name
-  address_prefixes     = "var.subnet_prefix" # Change to your desired subnet prefix
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.learningt3rraf0rm.name
+  virtual_network_name = azurerm_virtual_network.T3rraNet.name
+  address_prefixes     = var.subnet_prefix
 }
 
 # Define a Network Interface
 resource "azurerm_network_interface" "T3rra-nic" {
-  name                = "var.network_interface_name"
-  location            = azurerm_resource_group.T3rra-nic.location
-  resource_group_name = azurerm_resource_group.T3rra-nic.name
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.learningt3rraf0rm.location
+  resource_group_name = azurerm_resource_group.learningt3rraf0rm.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.SubT3rra.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Define the Azure Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "virtual-machine" {
-  name                = "var.vm_name" # Change to your desired VM name
-  resource_group_name = azurerm_resource_group.virtual-machine.name
-  location            = azurerm_resource_group.virtual-machine.location
-  size                = "var.vm_size" # Change to your desired VM size
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.learningt3rraf0rm.name
+  location            = azurerm_resource_group.learningt3rraf0rm.location
+  size                = var.vm_size
 
-  admin_username      = "var.admin_username" # Change to your desired admin username
-  admin_password      = "var.admin_password" # Set a strong password
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 
   network_interface_ids = [azurerm_network_interface.T3rra-nic.id]
 
@@ -51,13 +51,12 @@ resource "azurerm_linux_virtual_machine" "virtual-machine" {
   }
 
   source_image_reference {
-    publisher = "var.image_publisher" # Change to the image publisher
-    offer     = "var.image_offer"  # Change to the image offer
-    sku       = "var.image_sku"  # Change to the image SKU
-    version   = "var.image_version"  # Change to the desired version
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
 
-  tags = {
-    Name = "var.tags" # Change tags as needed
-  }
+  tags = var.tags
 }
+
